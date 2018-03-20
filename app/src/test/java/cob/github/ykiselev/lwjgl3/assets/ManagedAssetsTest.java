fed0a4f48f0215e3323ba0dc0d5c77344b0459bc
Uze <Uze@yandex.ru>
2018-03-20 23:08:02 +0300

    cleanup and unit test

--- app/src/test/java/cob/github/ykiselev/lwjgl3/assets/ManagedAssetsTest.java
+++ app/src/test/java/cob/github/ykiselev/lwjgl3/assets/ManagedAssetsTest.java
@@ -1,25 +1,9 @@
-/*
- * Copyright 2017 Yuriy Kiselev (uze@yandex.ru)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package cob.github.ykiselev.lwjgl3.assets;
 
-package com.github.ykiselev.assets;
-
-import org.junit.Test;
+import com.github.ykiselev.assets.Assets;
+import org.junit.jupiter.api.Test;
 
 import java.io.Closeable;
-import java.util.HashMap;
 import java.util.Optional;
 
 import static org.junit.Assert.assertSame;
@@ -32,17 +16,14 @@ import static org.mockito.Mockito.when;
 /**
  * @author Yuriy Kiselev (uze@yandex.ru).
  */
-public class ManagedAssetsTest {
+class ManagedAssetsTest {
 
     private Assets delegate = mock(Assets.class);
 
-    private final ManagedAssets assets = new ManagedAssets(
-            delegate,
-            new HashMap<>()
-    );
+    private final ManagedAssets assets = new ManagedAssets(delegate);
 
     @Test
-    public void shouldLoadOnce() {
+    void shouldLoadOnce() {
         when(delegate.tryLoad(eq("a"), eq(String.class), eq(assets)))
                 .thenReturn(Optional.of("A"));
         assertSame(
@@ -52,21 +33,21 @@ public class ManagedAssetsTest {
     }
 
     @Test
-    public void shouldCloseAutoCloseables() throws Exception {
+    void shouldCloseAutoCloseables() throws Exception {
         final AutoCloseable a = mock(AutoCloseable.class);
         when(delegate.tryLoad(eq("ac"), eq(AutoCloseable.class), eq(assets)))
                 .thenReturn(Optional.of(a));
-        assertSame(a, assets.load("ac", AutoCloseable.class));
+        assets.load("ac", AutoCloseable.class).close();
         assets.close();
         verify(a, atLeast(1)).close();
     }
 
     @Test
-    public void shouldCloseCloseables() throws Exception {
+    void shouldCloseCloseables() throws Exception {
         final Closeable c = mock(Closeable.class);
         when(delegate.tryLoad(eq("c"), eq(Closeable.class), eq(assets)))
                 .thenReturn(Optional.of(c));
-        assertSame(c, assets.load("c", Closeable.class));
+        assets.load("c", Closeable.class).close();
         assets.close();
         verify(c, atLeast(1)).close();
     }
